f89b97e01e9b3834c69c75865b7eae02c15d230f
algspd <algspd@gmail.com>
2013-07-17 12:23:12 +0200

    Implemented upload

--- octoscript/octoscript.sh
+++ octoscript/octoscript.sh
@@ -95,8 +95,16 @@ upload)
     usage
   fi
   arg_file=$4
-
-  echo "Not yet implemented"
+  
+  if [[ $arg_file != *.gcode ]] && [[ $arg_file != *.g ]];then
+    echo 'WARNING: You should name files like "something.gcode" or "something.g"'
+  fi
+  
+  curl "http://$arg_host:$arg_port/ajax/gcodefiles/upload"  -F gcode_file=@"$arg_file" >& /dev/null
+  
+  if [[ $? == 26 ]];then
+    echo "ERROR: Couldn't open file $arg_file"
+  fi
 ;;
 
 #############################################################
